71affd07975e679994e9be0f72cca9a7d6d4b138
10mi8o <totoma855@gmail.com>
2019-01-15 08:05:52 +0900

    データの書き込み確認

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+
+$dataFile = 'bbs.dat';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+  $content = $_POST['content'];
+  $user = $_POST['user'];
+
+  $newDate = $content . "\t" . $user . "\n";
+}
+
+$fp = fopen($dataFile, 'a');
+fwrite($fp, $newDate);
+fclose($fp);
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,7 +28,7 @@
 <body>
   <div class="container">
     <h1 class="heading">掲示板</h1>
-    <form class="formArea" action="">
+    <form class="formArea" method="post">
       <div class="inputField">
         <label for="content">content: </label><input id="content" type="text" name="content">
       </div>
@@ -19,7 +37,7 @@
       </div>
       <input type="submit" value="投稿する">
     </form>
-    <h2>一覧</h2>
+    <h2 class="subHeading">一覧</h2>
     <ul>
       <li>投稿はありません</li>
     </ul>
